887fbbe8591e9c5e75d866357b00dffc6a023081
Karula-k <68110808+Karula-k@users.noreply.github.com>
2024-12-24 22:37:57 +0700

    try changing ci-cd to bun (#3)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  output: 'export',
-  basePath: '/https://github.com/Karula-k/Karula-k.github.io',
+ basePath: '/https://github.com/Karula-k/Karula-k.github.io',
 };
 
 export default nextConfig;
