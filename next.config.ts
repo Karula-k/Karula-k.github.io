2f072e4e0a43e498bacd3a948aee72f937d35ced
Karula-k <d.fikriirsyad@gmail.com>
2024-12-24 23:37:03 +0700

    update next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export',
+    basePath: '',
+    distDir:'dist',
+    reactStrictMode: false,
 };
 
 export default nextConfig;
