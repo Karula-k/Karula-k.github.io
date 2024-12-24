35982e59d0ccdb18a5036b0816326ab5bba235ca
Karula-k <d.fikriirsyad@gmail.com>
2024-12-24 23:18:29 +0700

    hot fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- output: 'export',
- basePath: '/https://github.com/Karula-k/Karula-k.github.io',
 };
 
 export default nextConfig;
