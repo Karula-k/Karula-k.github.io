f61d8d075d754c830828ae9e658bb011830309ee
Karula-k <68110808+Karula-k@users.noreply.github.com>
2024-12-27 03:14:15 +0700

    Feat/update porto (#4)
    * git ignore
    
    * icons
    
    * update spacing and half scrolling
    
    * settings

--- app/components/CardWithDetail.tsx
+++ app/components/CardWithDetail.tsx
@@ -1,34 +1,35 @@
-'use client';
-import * as React from 'react';
-import { styled, Theme } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { blue,green, orange } from '@mui/material/colors';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkIcon from '@mui/icons-material/Link';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { PortfolioCardProps } from '../type/portofolioCardProps';
-import { Box } from '@mui/material';
+"use client";
+import * as React from "react";
+import { styled, Theme } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { blue, green, orange } from "@mui/material/colors";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkIcon from "@mui/icons-material/Link";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { PortfolioCardProps } from "../type/portofolioCardProps";
+import { Box } from "@mui/material";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
 
 const ExpandMore = styled((props: ExpandMoreProps) => {
-  const {  ...other } = props;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }: { theme: Theme; expand: boolean }) => ({
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
-  transform: expand ? 'rotate(180deg)' : 'rotate(0deg)',
+  transform: expand ? "rotate(180deg)" : "rotate(0deg)",
 }));
 
 export default function CardWithDetail({
@@ -47,17 +48,17 @@ export default function CardWithDetail({
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-   let avatarColor;
+  let avatarColor;
 
   switch (type) {
     case "FE":
       avatarColor = green[500];
       break;
     case "BE":
-      avatarColor = orange[500]; 
+      avatarColor = orange[500];
       break;
     case "ML":
-      avatarColor = blue[500]; 
+      avatarColor = blue[500];
       break;
     default:
       avatarColor = blue[500];
@@ -69,31 +70,31 @@ export default function CardWithDetail({
         avatar={
           <Box
             sx={{
-              width: 40, 
+              width: 40,
               height: 40,
-              bgcolor: avatarColor, 
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: '50%' 
+              bgcolor: avatarColor,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
             }}
-     >
-      <Typography variant="subtitle1" sx={{ color: 'white' }}>
-        {type?.toString()}
-      </Typography>
-    </Box>
+          >
+            <Typography variant="subtitle1" sx={{ color: "white" }}>
+              {type?.toString()}
+            </Typography>
+          </Box>
         }
         title={projectName}
         subheader={projectDate}
       />
       <CardMedia
         component="img"
-        sx={{ height: '140px', objectFit: 'contain' }}  
+        sx={{ height: "140px", objectFit: "contain" }}
         image={image}
         alt="Project Screenshot"
       />
       <CardContent>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
           {projectDescription}
         </Typography>
       </CardContent>
@@ -104,8 +105,12 @@ export default function CardWithDetail({
             <GitHubIcon />
           </IconButton>
         )}
-        {!isNDA && liveDemoLink && (
-          <IconButton aria-label="Live Demo" href={liveDemoLink} target="_blank">
+        {liveDemoLink && (
+          <IconButton
+            aria-label="Live Demo"
+            href={liveDemoLink}
+            target="_blank"
+          >
             <LinkIcon />
           </IconButton>
         )}
@@ -122,10 +127,10 @@ export default function CardWithDetail({
       <Collapse in={expanded && expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography sx={{ marginBottom: 2 }}>Tech Stack:</Typography>
-          <ul style={{ paddingLeft: '20px' }}>
+          <ul style={{ paddingLeft: "20px" }}>
             {techStack.map((tech, index) => (
-              <li key={index} style={{ marginBottom: '5px' }}>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              <li key={index} style={{ marginBottom: "5px" }}>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
                   • {tech}
                 </Typography>
               </li>
